8be544d81505d1068804603713895c664242ae83
Paul Pfeiffer <pfeiffer.paul@googlemail.com>
2017-10-12 22:19:36 +0200

    Initial Commit

---
+++ DataTrackerFramework/DataTrackerFramework.h
@@ -0,0 +1,19 @@
+//
+//  DataTrackerFramework.h
+//  DataTrackerFramework
+//
+//  Created by Paul Pfeiffer on 12.10.17.
+//  Copyright © 2017 Paul Pfeiffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataTrackerFramework.
+FOUNDATION_EXPORT double DataTrackerFrameworkVersionNumber;
+
+//! Project version string for DataTrackerFramework.
+FOUNDATION_EXPORT const unsigned char DataTrackerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataTrackerFramework/PublicHeader.h>
+
+
