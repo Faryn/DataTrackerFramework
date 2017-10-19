fe7d8369dedc9cccae6e17f9766aacc6b9382dfe
Paul Pfeiffer <pfeiffer.paul@googlemail.com>
2017-10-19 21:32:00 +0200

    Improvements to Today Widget

--- DataTrackerFramework/TrafficService.swift
+++ DataTrackerFramework/TrafficService.swift
@@ -21,8 +21,11 @@ class TrafficService {
     var trafficStats : TrafficStats?
     
     func getTrafficStats(_ completion: @escaping (_ stats: TrafficStats?, _ error: NSError?) -> ())  {
-        let url = URL(string: "https://blog.thepowl.de/test.json")!
-        let request = URLRequest(url: url)
+        //let url = URL(string: "https://blog.thepowl.de/test.json")!
+        let url = URL(string: "http://pass.telekom.de/api/service/generic/v1/status.json")!
+
+        var request = URLRequest(url: url)
+        request.setValue("Mozilla/5.0 (iPhone; CPU iPhone OS 11_1 like Mac OS X) AppleWebKit/604.3.3 (KHTML, like Gecko) Version/11.0 Mobile/15B5078e Safari/604.1", forHTTPHeaderField: "User-Agent")
         self.session.dataTask(with: request) { data, response, error in
             do {
                 let statsDictionary = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary
--- DataTrackerFramework/TrafficStats.swift
+++ DataTrackerFramework/TrafficStats.swift
@@ -17,6 +17,16 @@ open class TrafficStats {
     open var remainingTimeString : String?
     open var usedVolumeString: String?
     open var initialVolumeString: String?
+    open var remainingTimeSeconds: Int?
+    open var nextUpdate: Int?
+    open var usedPercentage: Int?
+    open var subscriptions: NSArray?
+    open var initialVolume: Int?
+    open var downSpeed: Int?
+    open var upSpeed: Int?
+    open var downSpeedString: String?
+    open var upSpeedString: String?
+    
 
     
     public init(fromDictionary: NSDictionary) {
@@ -24,6 +34,17 @@ open class TrafficStats {
         remainingTimeString = trafficStats?.value(forKey: "remainingTimeStr") as? String
         usedVolumeString = trafficStats?.value(forKey: "usedVolumeStr") as? String
         initialVolumeString = trafficStats?.value(forKey: "initialVolumeStr") as? String
+        remainingTimeSeconds = trafficStats?.value(forKey: "remainingSeconds") as? Int
+        nextUpdate = trafficStats?.value(forKey: "nextUpdate") as? Int
+        usedPercentage = trafficStats?.value(forKey: "usedPercentage") as? Int
+        subscriptions = trafficStats?.value(forKey: "subscriptions") as? NSArray
+        initialVolume = trafficStats?.value(forKey: "initialVolume") as? Int
+        downSpeed = trafficStats?.value(forKey: "downSpeed") as? Int
+        upSpeed = trafficStats?.value(forKey: "upSpeed") as? Int
+        downSpeedString = trafficStats?.value(forKey: "downSpeedStr") as? String
+        upSpeedString = trafficStats?.value(forKey: "upSpeedStr") as? String
+        
+        
     }
     
 }
